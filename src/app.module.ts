12cc931d0926b091b3c2c7ce249e4cd9370b9a84
Tilav <shavqiddintilovov0@gmail.com>
2026-02-27 17:14:29 +0500

    .

--- src/app.module.ts
+++ src/app.module.ts
@@ -28,7 +28,7 @@ import { APP_GUARD } from '@nestjs/core';
       {
         name: 'default',
         ttl: 60000,
-        limit: 20,
+        limit: 30,
       },
     ]),
     BullModule.forRootAsync({
