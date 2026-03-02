253cdb5266df5b36be5360a899f2bbb3fd2ed3da
Tilav <shavqiddintilovov0@gmail.com>
2026-03-02 14:55:09 +0500

    .

--- src/modules/auth/utils/regex-phone.ts
+++ src/modules/auth/utils/regex-phone.ts
@@ -1,7 +1,8 @@
 export const regexPhone = (phone: string) => {
   const cleanPhone = phone.replace(/\D/g, '');
 
-  const uzPhoneRegex = /^998(90|91|93|94|95|97|98|99|88|20)\d{7}$/;
+  const uzPhoneRegex =
+    /^998(33|50|70|75|77|78|88|90|91|93|94|95|97|98|99|20)\d{7}$/;
 
   const test = uzPhoneRegex.test(cleanPhone);
 
