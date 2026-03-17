ca3dbf9e64c1212af4357334f37f136653e3244c
Tilav <shavqiddintilovov0@gmail.com>
2026-03-17 10:41:28 +0500

    .

--- src/modules/sms/sms.module.ts
+++ src/modules/sms/sms.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { SmsService } from './sms.service';
+import { HttpModule } from '@nestjs/axios';
 @Module({
-  imports: [],
+  imports: [HttpModule],
   providers: [SmsService],
   exports: [SmsService],
 })
